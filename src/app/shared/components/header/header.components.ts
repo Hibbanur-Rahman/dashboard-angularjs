05e5716fc3048e70081327838826e5fde857df23
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2026-01-13 18:46:50 +0530

    adding the shared with components

---
+++ src/app/shared/components/header/header.components.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    imports:[],
+    selector:'app-header',
+    templateUrl:'./header.components.html',
+    styleUrls:['./header.components.css']
+})
+export class HeaderComponents {
+
+}
---
+++ src/app/shared/components/sidebar/sidebar.components.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    imports: [],
+    selector:'app-sidebar',
+    templateUrl:'./sidebar.components.html',
+    styleUrls:['./sidebar.components.css']
+})
+export class SidebarComponents {
+
+}
